f5c72002afc467b32d99bf1012a177aef83c8983
Eric Wyne <ecwyne@gmail.com>
2021-09-24 07:46:02 -0700

    include @mui/lab example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import { Container, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Button, Box } from '@mui/material';
+import Message from '@mui/icons-material/Message';
+import Download from '@mui/icons-material/Download';
 import { useSnackbar } from 'notistack';
+import { LoadingButton } from '@mui/lab';
 
 const Page = () => {
+    const [loading, setLoading] = useState(false);
     const { enqueueSnackbar } = useSnackbar();
     return (
         <Container maxWidth="lg">
-            <Button
-                color="primary"
-                variant="contained"
-                onClick={() =>
-                    enqueueSnackbar('This is a snackbar', {
-                        variant: 'success',
-                    })
-                }
-            >
-                Open Snackbar
-            </Button>
+            <Box mt={3} textAlign="center">
+                <Button
+                    color="success"
+                    variant="contained"
+                    endIcon={<Message />}
+                    onClick={() =>
+                        enqueueSnackbar('This is a snackbar', {
+                            variant: 'success',
+                        })
+                    }
+                >
+                    Open Snackbar
+                </Button>
+            </Box>
+            <Box mt={3} textAlign="center">
+                <LoadingButton
+                    loading={loading}
+                    loadingPosition="end"
+                    color="success"
+                    variant="contained"
+                    endIcon={<Download />}
+                    onClick={() => setLoading(true)}
+                >
+                    Download
+                </LoadingButton>
+            </Box>
         </Container>
     );
 };
