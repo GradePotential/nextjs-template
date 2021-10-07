5c6b85673dcb2e150454262f08723a58ad0f32ee
Eric Wyne <ecwyne@gmail.com>
2021-10-07 08:43:38 -0700

    add helpers for inferring query input/output

--- lib/trpc.ts
+++ lib/trpc.ts
@@ -1,7 +1,16 @@
 import { createReactQueryHooks } from '@trpc/react';
+import { inferProcedureInput, inferProcedureOutput } from '@trpc/server';
 import type { AppRouter } from 'pages/api/trpc/[trpc]';
 
 export const trpc = createReactQueryHooks<AppRouter>();
 export const useQuery = trpc.useQuery;
 export const useMutation = trpc.useMutation;
 export const useInfiniteQuery = trpc.useInfiniteQuery;
+
+export type inferQueryOutput<
+    TRouteKey extends keyof AppRouter['_def']['queries'],
+> = inferProcedureOutput<AppRouter['_def']['queries'][TRouteKey]>;
+
+export type inferQueryInput<
+    TRouteKey extends keyof AppRouter['_def']['queries'],
+> = inferProcedureInput<AppRouter['_def']['queries'][TRouteKey]>;
