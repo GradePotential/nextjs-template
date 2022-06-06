b2520e3be2ca5c61b25f1db8198a606b37e1d74c
Eric Wyne <ecwyne@gmail.com>
2022-06-06 07:11:54 -0700

    layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { trpc } from 'lib/trpc';
+import { Layout } from 'components/Layout';
 
 const Page = () => {
     const hello = trpc.useQuery(['hello']);
     return (
-        <div className="mt-4 text-center text-xl font-bold capitalize">
-            hello {hello.data?.name ?? 'Unknown'}
-        </div>
+        <Layout>
+            <div className="mt-4 text-center text-xl font-bold capitalize">
+                hello {hello.data?.name ?? 'Unknown'}
+            </div>
+        </Layout>
     );
 };
 
